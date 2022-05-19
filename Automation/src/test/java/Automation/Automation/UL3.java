31d35eb68b8a5b096c4194eb223c33ce69daae7d
mindsdet136 <mindsdet136@mindc1d185.makemylabs.in>
2022-05-19 09:56:39 +0530

    First commit

---
+++ Automation/src/test/java/Automation/Automation/UL3.java
@@ -0,0 +1,52 @@
+package Automation.Automation;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class UL3 {
+
+	public static void main(String[] args) throws InterruptedException {
+System.setProperty("webdriver.chrome.driver","C:\\Users\\mindsdet136\\Desktop\\COMPRIHENSIVE\\Q1\\SQ5\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.urbanladder.com/");
+		
+		
+		
+		
+		// 1) LOGIN 
+		
+				Actions a = new Actions(driver);
+				
+				
+				a.moveToElement(driver.findElement(By.xpath("//span[@class='header-icon-link user-profile-icon']"))).build().perform();
+				
+				a.moveToElement(driver.findElement(By.xpath("//a[@class='login-link authentication_popup']"))).click().build().perform();
+				
+				Thread.sleep(3000);
+				driver.findElement(By.xpath("(//input[@name='spree_user[email]'])[2]")).sendKeys("pundsahil09@gmail.com");
+				
+				driver.findElement(By.xpath("(//input[@name='spree_user[password]'])[3]")).sendKeys("Sahilpund100@");
+				
+				a.moveToElement(driver.findElement(By.xpath("(//input[@name='commit'])[3]"))).click().build().perform();	
+			
+				Thread.sleep(3000);
+				
+			
+				
+			//	3) Track Orders
+				
+				a.moveToElement(driver.findElement(By.xpath("//a[@title='Track Order']"))).click().build().perform();
+				Thread.sleep(3000);
+		
+	
+		
+		
+	}
+
+}
